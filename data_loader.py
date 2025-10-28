250d5b53ecd9bf669c416edab706d04ae9b2598e
nastjannastja <verdenerliten@vk.com>
2025-10-28 22:05:01 +0300

    Add lines to data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -6,6 +6,16 @@ file_url = f"https://drive.google.com/uc?id={file_id}"
 
 raw_data = pd.read_csv(file_url)
 
+raw_data.rename(
+    columns={"Have you ever had suicidal thoughts ?": "Suicidal_Thoughts"}, inplace=True
+)
+
+raw_data.columns = (
+    raw_data.columns.str.strip()
+    .str.replace(" ", "_", regex=False)
+    .str.replace("/", "_or_", regex=False)
+)
+
 raw_data = raw_data.astype(
     {
         "id": "Int32",
@@ -13,21 +23,20 @@ raw_data = raw_data.astype(
         "Age": "Int32",
         "City": "string",
         "Profession": "string",
-        "Academic Pressure": "Int32",
-        "Work Pressure": "Int32",
+        "Academic_Pressure": "Int32",
+        "Work_Pressure": "Int32",
         "CGPA": "float32",
-        "Study Satisfaction": "Int32",
-        "Job Satisfaction": "Int32",
-        "Sleep Duration": "string",
-        "Dietary Habits": "string",
+        "Study_Satisfaction": "Int32",
+        "Job_Satisfaction": "Int32",
+        "Sleep_Duration": "string",
+        "Dietary_Habits": "string",
         "Degree": "string",
-        "Have you ever had suicidal thoughts ?": "string",
-        "Work/Study Hours": "Int32",
-        "Financial Stress": "string",
-        "Family History of Mental Illness": "string",
+        "Suicidal_Thoughts": "bool",
+        "Work_or_Study_Hours": "Int32",
+        "Financial_Stress": "string",
+        "Family_History_of_Mental_Illness": "bool",
         "Depression": "int8",
     }
 )
 print(raw_data.dtypes)
 raw_data.to_parquet("dataset.parquet", engine="pyarrow", index=False)
-
